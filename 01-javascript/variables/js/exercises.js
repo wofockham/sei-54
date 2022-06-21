e90085077900da766720cdf326e086332388bfc6
wofockham <certainthings@gmail.com>
2022-06-21 15:12:07 +1000

    js variable solutions ii

--- 01-javascript/variables/js/exercises.js
+++ 01-javascript/variables/js/exercises.js
@@ -57,7 +57,6 @@ const yearsRemaining = deathAge - age;
 const amountRequired = yearsRemaining * amountPerYear;
 console.log(`You will need ${ amountRequired } to last you until the ripe old age of ${ deathAge }`);
 
-
 // ## The Geometrizer
 
 // Calculate properties of a circle, using [the definitions here](http://math2.org/math/geometry/circles.htm).
@@ -65,6 +64,12 @@ console.log(`You will need ${ amountRequired } to last you until the ripe old ag
 // - Store a radius into a variable.
 // - Calculate the circumference based on the radius, and output "The circumference is NN".
 // - Calculate the area based on the radius, and output "The area is NN".
+
+const radius = 1;
+const circumference = 2 * radius * Math.PI; // diameter * pi
+console.log(`The circumference is ${ circumference }`);
+const area = Math.PI * radius * radius; // pi * radius^2
+console.log(`The area is ${ area }`);
 
 // ## The Temperature Converter
 
@@ -74,3 +79,10 @@ console.log(`You will need ${ amountRequired } to last you until the ripe old ag
 // - Convert it to fahrenheit and output "NN°C is NN°F".
 // - Now store a fahrenheit temperature into a variable.
 // - Convert it to celsius and output "NN°F is NN°C."
+
+const currentTemperature = 20; // celsius
+const fahrenheit = currentTemperature * 1.8 + 32;
+console.log(`${ currentTemperature }°C is ${ fahrenheit }°F`);
+
+const celsius = (fahrenheit - 32) / 1.8; // precedence
+console.log(`${ fahrenheit }°F is ${ celsius }°C.`);

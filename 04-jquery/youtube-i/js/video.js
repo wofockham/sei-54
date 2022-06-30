857c79d8446c72d31c680d65d7f424fe99e13d3b
wofockham <certainthings@gmail.com>
2022-06-30 12:17:04 +1000

    youtube library

---
+++ 04-jquery/youtube-i/js/video.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('a');
+
+for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+
+    const href = link.getAttribute('href');
+    const thumbnailURL = youtube.generateThumbnailUrl(href);
+
+    const image = document.createElement('img'); // detached DOM node
+    image.setAttribute('src', thumbnailURL);
+
+    link.appendChild(image);
+}
+

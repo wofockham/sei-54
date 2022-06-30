eb1987138c6266ebe7cbaa3edd14678d03a6d3f2
wofockham <certainthings@gmail.com>
2022-06-30 15:27:59 +1000

    embedded youtube player with jquery

---
+++ 04-jquery/youtube-iii/js/video.js
@@ -0,0 +1,25 @@
+const $links = $('a');
+
+// Thumbnails
+for (let i = 0; i < $links.length; i++) {
+    const link = $links[i]; // retrieve the vanilla DOM node
+    const $link = $(link);  // turn the DOM node back into a jQuery object
+
+    const href = $link.attr('href'); // getter
+    const thumbnailURL = youtube.generateThumbnailUrl(href);
+
+    const $image = $('<img>'); // detached jQuery object
+    $image.attr('src', thumbnailURL); // setter
+    $link.prepend($image);
+}
+
+// Video player
+$links.on('click', function (event) {
+    event.preventDefault(); // Disables the browser's default behaviour (don't follow the link!)
+
+    const href = $(this).attr('href');
+    const embedURL = youtube.generateEmbedUrl(href);
+
+    const embedCode = `<iframe width="560" height="315" src="${ embedURL }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    $('#player').hide().html(embedCode).fadeIn(5000); // chaining
+});

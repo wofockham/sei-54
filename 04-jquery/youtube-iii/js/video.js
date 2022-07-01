feeb1839267174b77784daddbd3af351193bc9b8
wofockham <certainthings@gmail.com>
2022-07-01 14:28:46 +1000

    document ready

--- 04-jquery/youtube-iii/js/video.js
+++ 04-jquery/youtube-iii/js/video.js
@@ -1,25 +1,31 @@
-const $links = $('a');
+$(document).ready(function () {
+    const $links = $('a');
+    console.log($links.length);
 
-// Thumbnails
-for (let i = 0; i < $links.length; i++) {
-    const link = $links[i]; // retrieve the vanilla DOM node
-    const $link = $(link);  // turn the DOM node back into a jQuery object
+    // Thumbnails
+    for (let i = 0; i < $links.length; i++) {
+        const link = $links[i]; // retrieve the vanilla DOM node
+        const $link = $(link);  // turn the DOM node back into a jQuery object
 
-    const href = $link.attr('href'); // getter
-    const thumbnailURL = youtube.generateThumbnailUrl(href);
+        const href = $link.attr('href'); // getter
+        const thumbnailURL = youtube.generateThumbnailUrl(href);
 
-    const $image = $('<img>'); // detached jQuery object
-    $image.attr('src', thumbnailURL); // setter
-    $link.prepend($image);
-}
+        const $image = $('<img>'); // detached jQuery object
+        $image.attr('src', thumbnailURL); // setter
+        $link.prepend($image);
+    }
 
-// Video player
-$links.on('click', function (event) {
-    event.preventDefault(); // Disables the browser's default behaviour (don't follow the link!)
+    // Video player
+    $links.on('click', function (event) {
+        event.preventDefault(); // Disables the browser's default behaviour (don't follow the link!)
 
-    const href = $(this).attr('href');
-    const embedURL = youtube.generateEmbedUrl(href);
+        const href = $(this).attr('href');
+        const embedURL = youtube.generateEmbedUrl(href);
 
-    const embedCode = `<iframe width="560" height="315" src="${ embedURL }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-    $('#player').hide().html(embedCode).fadeIn(5000); // chaining
+        const embedCode = `<iframe width="560" height="315" src="${ embedURL }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        $('#player').hide().html(embedCode).fadeIn(5000); // chaining
+    });
+
+    $('h2').funText(100, 'candy');
+    $('a').funText(50, 'reverseRainbow');
 });

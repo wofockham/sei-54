4e3805a9c86763624785b37ed63d0bcf61b1498c
wofockham <certainthings@gmail.com>
2022-07-13 13:01:47 +1000

    calculator in sinatra

---
+++ 06-sinatra/calculator/main.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+# ERB: Embedded Ruby
+
+get '/' do
+    erb :home
+end
+
+get '/result' do
+    x = params[:x].to_f
+    y = params[:y].to_f
+
+    @result = case params[:operator]
+    when '+' then x + y
+    when '-' then x - y
+    when '*' then x * y
+    when '/' then x / y
+    end
+
+    erb :result
+end

277c7b32cf7c76fd74f19004eab32ac1abe68035
wofockham <certainthings@gmail.com>
2022-07-14 11:07:17 +1000

    an introduction to the horrors of SQL

---
+++ 07-databases/intro/create_pets.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pets (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER
+);
+
+-- seed data
+INSERT INTO pets (id, name, breed, age) VALUES (1, 'Fluffy', 'unicorn', 1001);
+INSERT INTO pets (id, name, breed, age) VALUES (2, 'Gigantor', 'robot', 1);

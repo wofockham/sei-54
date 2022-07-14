a619592d6142c32a2565d4445f5106779cadafa2
wofockham <certainthings@gmail.com>
2022-07-14 13:00:07 +1000

    butterfly show part i

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,8 +8,22 @@ end
 
 # INDEX
 get '/butterflies' do
+    @butterflies = query_db 'SELECT * FROM butterflies'
+    erb :butterflies_index
+end
+
+# SHOW
+get '/butterflies/:id' do
+    @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+    @butterfly = @butterfly.first # Pluck the single butterfly from the array
+    erb :butterflies_show
+end
+
+def query_db(sql_statement)
+    puts sql_statement # Optional but nice for debugging
     db = SQLite3::Database.new 'database.sqlite3'
     db.results_as_hash = true
-    @butterflies = db.execute 'SELECT * FROM butterflies'
-    erb :butterflies_index
+    results = db.execute sql_statement
+    db.close
+    results # implicit return
 end

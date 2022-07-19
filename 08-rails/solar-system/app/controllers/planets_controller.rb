5cbd55fb92b714901e604cc1ba6c031c864909e6
wofockham <certainthings@gmail.com>
2022-07-19 12:58:55 +1000

    planets #new

--- 08-rails/solar-system/app/controllers/planets_controller.rb
+++ 08-rails/solar-system/app/controllers/planets_controller.rb
@@ -2,4 +2,11 @@ class PlanetsController < ApplicationController
     def index
         @planets = Planet.all
     end
+
+    def show
+        @planet = Planet.find params[:id]
+    end
+
+    def new
+    end
 end
--- 08-rails/solar-system/config/routes.rb
+++ 08-rails/solar-system/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # This file will cause the most suffering today
+  root :to => 'planets#index'
   get '/planets' => 'planets#index'
+  get '/planets/new' => 'planets#new', :as => :new_planet
   get '/planets/:id' => 'planets#show', :as => :planet
 end

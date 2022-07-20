f8dc593a185879e9d793f68b7b997e63663063e4
wofockham <certainthings@gmail.com>
2022-07-20 13:09:13 +1000

    artists CRUD

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -4,11 +4,37 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist
   end
 
   def edit
+    @artist = Artist.find params[:id]
+  end
+
+  def update
+    artist = Artist.find params[:id]
+    artist.update artist_params
+    redirect_to artist
   end
 
   def show
+    @artist = Artist.find params[:id]
+  end
+
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+  # Strong params
+  def artist_params
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
   end
 end

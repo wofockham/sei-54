9c68fcef1289547b2a0753010943e385e2a1683a
wofockham <certainthings@gmail.com>
2022-07-21 15:11:10 +1000

    sign up and sign in

--- 08-rails/tunr/app/controllers/application_controller.rb
+++ 08-rails/tunr/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :fetch_user
+
+    def fetch_user
+        @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+        session[:user_id] = nil unless @current_user.present? # Log out non-existent users.
+    end
 end

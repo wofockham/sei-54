f92a392e77f7a8ba1d0291fa9d00adf8ad84b53d
wofockham <certainthings@gmail.com>
2022-08-04 15:00:18 +1000

    fast fibonacci

--- 11-comp-sci/recursion/fibonacci.rb
+++ 11-comp-sci/recursion/fibonacci.rb
@@ -26,6 +26,25 @@ end
 # Bonus homework:
 # Make the recursive version faster!
 # Option a: memoisation
+
+def fib_memo(n)
+    @fib ||= {}  # assigns the empty hash to @fib if it doesn't already have a value   
+    if @fib[n]   # if we have previously saved this fibonacci number...
+        @fib[n]  # ... return that value immediately
+    elsif n <= 2
+        1
+    else
+        @fib[n] = fib_memo(n-1) + fib_memo(n-2) # save this fibonacci number for later use
+    end
+end
+
 # Option b: iterative recursion
+def fib(n, a=1, b=1)
+    if n == 1 || n == 2 # base case
+        b
+    else
+        fib n-1, b, a+b # Almost mystical
+    end
+end
 
 binding.pry

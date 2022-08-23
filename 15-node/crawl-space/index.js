486de98b35cf7ec74a5e6400033b44e98b5d5e30
wofockham <certainthings@gmail.com>
2022-08-23 12:44:51 +1000

    filter terrible sites

--- 15-node/crawl-space/index.js
+++ 15-node/crawl-space/index.js
@@ -1,21 +1,40 @@
 const Crawler = require('crawler');
 
+const isCrawlable = (url) => {
+    const terribleSites = ['archive', 'github.com', 'microsoft.com', 'twitter.com', 'sourceware.org'];
+
+    // magic
+    const terribleSiteDetected = terribleSites.some((awfulSite) => {
+        return url.includes(awfulSite);
+    });
+
+    return !terribleSiteDetected;
+
+    // 1 liner: heavy magic
+    // return ! terribleSites.some( awfulSite => url.includes(awfulSite) );
+};
+
 const c = new Crawler({
     rateLimit: 1000,
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:78.0) Gecko/20100101 Firefox/78.0',
     callback: (err, res, done) => {
-        console.log(res.request.uri.href); // the URL for the current page
-        const title = res.$('title').text();
-        console.log(title);
-        console.log(); // new line for readability
-
-        const $links = res.$('a[href^=http]'); // External sites only please
-        $links.each(function () {
-            const href = res.$(this).attr('href');
-            c.queue(href);
-        });
-        done();
+        if (typeof res.$ === 'function') {
+            console.log(res.request.uri.href); // the URL for the current page
+            const title = res.$('title').text();
+            console.log(title);
+            console.log(); // new line for readability
+    
+            const $links = res.$('a[href^=http]'); // External sites only please
+            $links.each(function () {
+                const href = res.$(this).attr('href');
+                if (isCrawlable(href)) {
+                    c.queue(href);
+                }
+            });
+        }
+        done(); // we must call this every time the callback runs
     }
 });
 
-c.queue('https://lobste.rs/');
+// process.argv contains command line arguments (and other crap)
+c.queue(process.argv[2] || 'https://lobste.rs/');

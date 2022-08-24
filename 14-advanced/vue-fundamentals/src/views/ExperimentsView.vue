1731b36a3c842a22099255b1197c0ae887b33fae
wofockham <certainthings@gmail.com>
2022-08-24 14:37:10 +1000

    vue SocialProfile

--- 14-advanced/vue-fundamentals/src/views/ExperimentsView.vue
+++ 14-advanced/vue-fundamentals/src/views/ExperimentsView.vue
@@ -6,16 +6,22 @@
 
         <hr />
 
+        <SocialProfile name="Groucho" bio="No gods no masters" pic="http://www.fillmurray.com/400/600" />
+        <SocialProfile name="Harpo" bio="No gods no masters" pic="http://www.fillmurray.com/401/600" />
+        <SocialProfile name="Chico" bio="No gods no masters" pic="http://www.fillmurray.com/400/601" />
+
     </div>
 </template>
 
 <script>
 // @ is a shortcut for `src`
 import ClickrButton from '@/components/ClickrButton.vue';
+import SocialProfile from '@/components/SocialProfile.vue';
 export default {
     name: 'ExperimentsView',
     components: {
-        ClickrButton: ClickrButton
+        ClickrButton: ClickrButton,
+        SocialProfile: SocialProfile
     }
 };
 </script>

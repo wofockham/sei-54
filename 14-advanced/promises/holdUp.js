d0675656b788f16c1a034fbaa5e0714a349c1dd3
wofockham <certainthings@gmail.com>
2022-08-25 11:28:57 +1000

    promisey settimeout

--- 14-advanced/promises/holdUp.js
+++ 14-advanced/promises/holdUp.js
@@ -2,8 +2,20 @@
 // task: wrap setTimeout in a Promise (so it works with .then)
 
 const holdUp = (duration) => {
-
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
 };
+
+// Alternatives:
+
+// No curlies: implicit return
+// const holdUp = (duration) => new Promise((resolve) => {
+//     setTimeout(resolve, duration);
+// });
+
+// No curlies, no return: implicit return all the way done:
+// const holdUp = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
 
 // Make this code work:
 holdUp(3000).then(() => {

c5fa6fc706f63f8649a29ad78f2943ece3694922
wofockham <certainthings@gmail.com>
2022-08-26 11:55:39 +1000

    paintr

--- 14-advanced/paintr/app/assets/javascripts/paintr.js
+++ 14-advanced/paintr/app/assets/javascripts/paintr.js
@@ -4,4 +4,17 @@ $(document).ready(function () {
         const $swatch = $('<div />').addClass('swatch').css('background-color', color);
         $swatch.appendTo('.palette'); // $('.palette').append($swatch)
     });
+
+    // Event delegation: listen for events on future DOM elements
+    $('.palette').on('click', '.swatch', function () {
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // Event delegation: listen for events on the parent FOR EFFICIENCY
+    $('.canvas').on('mouseover', '.pixel', function (event) {
+        if (event.shiftKey === false) return;
+        const color = $('.swatch.selected').css('background-color');
+        $(this).css('background-color', color);
+    });
 });

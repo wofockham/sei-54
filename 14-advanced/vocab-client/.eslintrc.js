1f920019cfcaa1b8ac4997f1e33a3da7713d72da
wofockham <certainthings@gmail.com>
2022-08-29 12:03:26 +1000

    vocab app.vue

---
+++ 14-advanced/vocab-client/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-mutating-props': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}

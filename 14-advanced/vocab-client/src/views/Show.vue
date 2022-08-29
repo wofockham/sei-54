f2ae857086da09d2d6e9f70b052c3144296ea1b0
wofockham <certainthings@gmail.com>
2022-08-29 12:48:09 +1000

    Show.vue

---
+++ 14-advanced/vocab-client/src/views/Show.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="ui labeled input fluid">
+            <div class="ui label">
+                <i class="germany flag" /> German
+            </div>
+            <input type="text" readonly :value="word.german">
+        </div>
+        <div class="ui labeled input fluid">
+            <div class="ui label">
+                <i class="united kingdom flag" /> English
+            </div>
+            <input type="text" readonly :value="word.english">
+        </div>
+        <div class="actions">
+            <router-link :to="{ name: 'edit', params: { id: this.$route.params.id } }">
+                Edit word
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import { api } from '../helpers/helpers';
+
+export default {
+    name: 'show',
+    data() {
+        return {
+            word: {}
+        };
+    },
+    async mounted() {
+        this.word = await api.getWord( this.$route.params.id );
+    }
+};
+</script>
+
+<style scoped>
+.actions a {
+   display: block;
+   text-decoration: underline;
+   margin: 20px 10px; 
+}
+</style>
